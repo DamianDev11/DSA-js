13f52ed8a859ad73f1ec3bd3a50dfca1211d666a
Damian <damianprakash07@gmail.com>
2023-08-18 20:48:01 +0530

    Arrays and various operations

---
+++ Arrays/code.js
@@ -0,0 +1,9 @@
+const products = ["fam0", "eam1", "dam2"];
+products.push = "gam0";
+const products1 = ["dalow"];
+products1.push("dalow1");
+console.log(products);
+console.log(products1);
+console.log(products.length);
+console.log(products1.length);
+console.log(products.sort());
