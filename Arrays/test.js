6b0fdcfe96abd6b29f0bf39866927b926f0ab0e4
Damian <damianprakash07@gmail.com>
2023-08-19 00:42:05 +0530

    remove Duplicates from Array

---
+++ Arrays/test.js
@@ -0,0 +1,14 @@
+const num1 = [0, 0, 0, 1, 1, 2, 3, 3, 3, 4, 5, 5, 6, 7, 7, 7, 7, 8, 9];
+const nums = num1.reverse();
+console.log(nums);
+nums.sort();
+const removeDup = [];
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] !== nums[i + 1]) {
+    removeDup.push(nums[i]);
+  }
+}
+
+console.log(nums);
+console.log(removeDup);
